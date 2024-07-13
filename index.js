e1766fb882cb69d7be23eadb2fe52ee228c5d799
Mungai Munene <mungai.munene@gmail.com>
2024-07-13 12:53:50 +0300

    completed functions lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+// saturdayFun
+function saturdayFun(target = "roller-skate") {
+    return `This Saturday, I want to ${target}!`
+}
+saturdayFun()
+
+// mondayWork
+function mondayWork(target = "go to the office") {
+    return `This Monday, I will ${target}.`
+}
+mondayWork()
+
+//wrapAdjective 
+function wrapAdjective(special ='*') {
+    return function(adjective) {
+        return `You are ${special}${adjective}${special}!`
+    }
+}
